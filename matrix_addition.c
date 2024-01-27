ab5a676818b0ff1f7f0f583dc2c5f886435587d0
Muhammad Tameem Gazi <116261279+Tameem2004@users.noreply.github.com>
2024-01-27 21:49:24 +0530

    Add files via upload

---
+++ matrix_addition.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 3
+void addition(int A[N][N],int B[N][N])
+{
+    int C[N][N],i,j;
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            C[i][j]=A[i][j]+B[i][j];
+            
+        }
+        printf("\n");
+    }
+
+    printf("MATRIX ADDITION IS: \n");
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            printf("%d ",C[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int arr1[N][N],arr2[N][N];
+    printf("Enter Values In 1st Matrix");
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+    printf("Enter Values In 2nd Matrix");
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<N;j++)
+        {
+            scanf("%d",&arr2[i][j]);
+        }
+    }
+    addition(arr1,arr2);
+}
