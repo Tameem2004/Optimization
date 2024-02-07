ff30387877e40b44eac126686dd8e6c22aa12af1
Muhammad Tameem Gazi <116261279+Tameem2004@users.noreply.github.com>
2024-02-07 20:37:27 +0530

    Update matrix_multiplication.c

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define N 3
-void multiplication(int A[N][N],int B[N][N])
+
+void multiplication(int A[10][10],int B[10][10],int r1,int c2,int c1)
 {
-    int D[N][N],i,j,k;
-    for(i=0;i<N;i++)
+    int D[10][10],i,j,k;
+    for(i=0;i<r1;i++)
     {
-        for(j=0;j<N;j++)
+        for(j=0;j<c2;j++)
         {
             D[i][j]=0;
-            for(k=0;k<N;k++)
+            for(k=0;k<c1;k++)
             {
                 D[i][j]=D[i][j]+A[i][k]*B[k][j];
             }
@@ -19,9 +19,9 @@ void multiplication(int A[N][N],int B[N][N])
     }
 
     printf("MATRIX MULTIPLICATION IS: \n");
-    for(i=0;i<N;i++)
+    for(i=0;i<r1;i++)
     {
-        for(j=0;j<N;j++)
+        for(j=0;j<c2;j++)
         {
             printf("%d ",D[i][j]);
         }
@@ -31,22 +31,39 @@ void multiplication(int A[N][N],int B[N][N])
 
 int main()
 {
-    int arr1[N][N],arr2[N][N];
-    printf("Enter Values In 1st Matrix");
-    for(int i=0;i<N;i++)
+    int r1,c1,r2,c2,i,j,ch;
+    printf("ENTER THE NUMBER OF ROWS AND COLUMNS IN 1ST ARRY");
+    scanf("%d%d",&r1,&c1);
+    printf("ENTER THE ROWS AND COLUMNS IN 2ND ARRAY");
+    scanf("%d%d",&r2,&c2);
+    int A[10][10],B[10][10],C[10][10];
+    
+    printf("ENTER NUMBER IN FIRST MATRIX\n");
     {
-        for(int j=0;j<N;j++)
+        for(i=0;i<r1;i++)
         {
-            scanf("%d",&arr1[i][j]);
+            for(j=0;j<c1;j++)
+            {
+                scanf("%d",&A[i][j]);
+            }
         }
     }
-    printf("Enter Values In 2nd Matrix");
-    for(int i=0;i<N;i++)
+
+    printf("ENTER NUMBER IN SECOND MATRIX\n");
     {
-        for(int j=0;j<N;j++)
+        for(i=0;i<r2;i++)
         {
-            scanf("%d",&arr2[i][j]);
+            for(j=0;j<c2;j++)
+            {
+                scanf("%d",&B[i][j]);
+            }
         }
     }
-    multiplication(arr1,arr2);
+    if(c1!=r2)
+        {
+            printf("\n YOUR ARRAY CANNOT BE MULTIPLIABLE");
+            exit(1);
+        }
+    else 
+        multiplication(A,B,r1,c2,c1);
 }
